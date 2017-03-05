f7b0cda85e21d4204062321caaeee220e4e0b751
Luis Fernando <lfbu.112@gmail.com>
2017-03-05 11:41:35 -0300

    first init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".menu a").smoothscrolling();
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$(".scrollup").fadeIn();
+		}else{
+			$(".scrollup").fadeOut();
+		}
+	})
+});
