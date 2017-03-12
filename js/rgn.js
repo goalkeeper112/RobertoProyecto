7d2ceafdc659809e2f21e26343a9ea42649d8fb4
Luis Fernando <lfbu.112@gmail.com>
2017-03-11 23:36:45 -0300

    test contact form

--- js/rgn.js
+++ js/rgn.js
@@ -8,4 +8,43 @@ $(document).ready(function(){
 			$(".scrollup").fadeOut();
 		}
 	})
+
+	$("#iconProduct1").on('mouseover', function(){
+		$(this).addClass("animated");
+		$(this).addClass("bounce");
+	})
+
+	$("#iconProduct1").on('mouseout', function(){
+		$(this).removeClass("animated");
+		$(this).removeClass("bounce");
+	})
+
+	$("#iconProduct2").on('mouseover', function(){
+		$(this).addClass("animated");
+		$(this).addClass("bounce");
+	})
+
+	$("#iconProduct2").on('mouseout', function(){
+		$(this).removeClass("animated");
+		$(this).removeClass("bounce");
+	})
+
+	$("#iconProduct3").on('mouseover', function(){
+		$(this).addClass("animated");
+		$(this).addClass("bounce");
+	})
+
+	$("#iconProduct3").on('mouseout', function(){
+		$(this).removeClass("animated");
+		$(this).removeClass("bounce");
+	})
+
+	
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
 });
